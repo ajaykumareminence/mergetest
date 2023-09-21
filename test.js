3570913b310e2202943cf82d5c21cdf59c4f7bd6
webexpert <ajaykumar1.eminence@gmail.com>
2023-09-21 12:14:59 +0530

    dev

---
+++ test.js
@@ -0,0 +1,10 @@
+import myJson from './data.json' assert {type: 'json'};
+
+const remove = ["supplierName", "noOfAdults", "noOfChilds"]
+const keys = Object.keys(myJson).map(v => {
+    if (!remove.includes(v)) {
+        return v
+    }
+})
+const fil = keys.filter((item)=>item!=undefined);
+console.log(fil)
